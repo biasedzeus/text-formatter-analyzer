f29b000497adf40bd09dd935abf28af5f0d57529
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-08 15:54:50 +0530

    fixed body theme toggling

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import TextForm from './Components/TextForm';
 
 
 function App() {
-  //to check system is set to dark or light theme 
-
+  //to check system is set to dark or light theme
   const isSystemDark = window.matchMedia("(prefers-color-scheme:dark)").matches
+
+  const [darkMode, setDarkMode] = useState(isSystemDark);
+
+
   console.log(isSystemDark)
+  
+  //to set bg of whole body based on theme toggle
+  if(darkMode){
+    document.body.style.backgroundColor="black"
+  }
+  else {
+    document.body.style.backgroundColor="white"
+  }
 
   const lightTheme = {
     backgroundColor : "white",
-    color : "black"
+    color : "black",
+  
+
   }
 
   const darkTheme = {
@@ -21,16 +34,22 @@ function App() {
     color : "white"
   }
 
-  const [darkMode, setDarkMode] = useState(isSystemDark);
 
   const toggleMode = () =>{
+    
     setDarkMode(toggle =>!toggle)
+
 
   }
 
   return (
     < div style={darkMode?darkTheme:lightTheme}>
-       <NavBar  mode={darkMode} toggleMode={toggleMode} toggler={darkMode?"Set Light Mode":"Set Dark Mode"}/>
+       <NavBar  mode={darkMode} 
+       toggleMode={toggleMode} 
+       toggler={darkMode?"Set Light Mode":"Set Dark Mode"}
+       mode ={darkMode?"dark":"light"}
+       
+       />
        
     // <div className="container">
     {/* <About/> */}
