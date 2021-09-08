68593128ee1f0ec39757c69cf435af0dde63d5ce
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-08 21:49:16 +0530

    AlerJS Condional Rendering
    Conditional Render Of Alert On top of TextForm using AND operator Ex.ex :props.alert&&<div>

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Alert(props) {
     return (
-        <div className="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong>{props.alert}
+        props.alert && <div className="alert alert-warning alert-dismissible fade show container fluid" role="alert">
+  <strong>{props.alert.type} !!! </strong> {props.alert.message}
   <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
     )
