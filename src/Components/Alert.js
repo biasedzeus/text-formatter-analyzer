cdc5807eb2de82f3ac1412e26cf599afb7865306
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-08 20:53:32 +0530

    added alertJS

---
+++ src/Components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Alert(props) {
+    return (
+        <div>
+            <div className="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> {props.alert}
+  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+            
+        </div>
+    )
+}
+
+export default Alert
